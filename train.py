6f1947a7251b07d929fb5e9110585b7864c61fe8
Habiba160 <s-habiba.ibrahim@zewailcity.edu.eg>
2026-03-26 06:21:52 +0200

    Fix pipeline: upload accuracy.txt and set fixed accuracy

--- train.py
+++ train.py
@@ -1,14 +1,12 @@
-
 # simulate training
 accuracy = 0.9
 
 print(f"Model accuracy: {accuracy}")
 
-# save accuracy to file (simulate MLflow)
+# save accuracy to file
 with open("accuracy.txt", "w") as f:
     f.write(str(accuracy))
 
 # simulate run ID
 with open("run_id.txt", "w") as f:
     f.write("12345")
-    
